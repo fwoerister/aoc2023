73705b09e75cb94ac4ef7ca8a80ec6344f21a010
Florian Wörister <florian.woerister@univie.ac.at>
2023-12-06 23:39:37 +0100

    refactored day_05

--- day_05.py
+++ day_05.py
@@ -3,37 +3,37 @@ import datetime
 
 
 class Range:
-    def __init__(self, start, end):
+    def __init__(self, start: int, end: int):
         self.start = start
         self.end = end
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.start} - {self.end}"
 
-    def intersects_with(self, other):
+    def intersects_with(self, other: "Range") -> bool:
         return self.start < other.end and self.end > other.start
 
-    def intersection(self, other):
+    def intersection(self, other: "Range") -> "Range":
         return Range(max(self.start, other.start), min(self.end, other.end))
 
-    def left_remainder(self, other):
+    def left_remainder(self, other: "Range") -> "Range":
         if self.start <= other.start - 1:
             return Range(self.start, other.start - 1)
 
-    def right_remainder(self, other):
+    def right_remainder(self, other: "Range") -> "Range":
         if other.end + 1 <= self.end:
             return Range(other.end + 1, self.end)
 
 
 class MappedRange(Range):
-    def __init__(self, start, end, offset):
+    def __init__(self, start: int, end: int, offset: int):
         super().__init__(start, end)
         self.offset = offset
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.start} - {self.end} ({self.offset})"
 
-    def translate_range(self, other: Range):
+    def translate_range(self, other: Range) -> (list, Range, list):
         if self.intersects_with(other):
             intersection = other.intersection(self)
             intersection.start += self.offset
@@ -45,29 +45,33 @@ class MappedRange(Range):
 
 
 class MappingStage:
-    def __init__(self, mapped_ranges):
+    def __init__(self, mapped_ranges: list):
         self.mapped_ranges = mapped_ranges
         self.mapped_ranges.sort(key=lambda element: element.start)
 
-    def translate(self, seed_ranges: list):
+    def apply(self, seed_ranges: list) -> list:
         result = []
         for seed_range in seed_ranges:
-            new_range_delta = 0
-            for mapped_range in self.mapped_ranges:
-                left_remainder, mapped_intersection, right_remainder = mapped_range.translate_range(seed_range)
-
-                if left_remainder:
-                    seed_ranges.append(left_remainder)
-                if right_remainder:
-                    seed_ranges.append(right_remainder)
-                if mapped_intersection:
-                    result.append(mapped_intersection)
-                    new_range_delta += 1
-
-            if new_range_delta == 0:
-                result.append(seed_range)
+            mapped_intersection, remainders = self._apply_mapped_ranges(seed_range)
+            seed_ranges.extend(remainders)
+            result.append(mapped_intersection if mapped_intersection is not None else seed_range)
 
         return result
+
+    def _apply_mapped_ranges(self, seed_range: Range) -> (Range, list):
+        for mapped_range in self.mapped_ranges:
+            left_remainder, mapped_intersection, right_remainder = mapped_range.translate_range(seed_range)
+
+            remainders = []
+            if left_remainder:
+                remainders.append(left_remainder)
+
+            if right_remainder:
+                remainders.append(right_remainder)
+
+            if mapped_intersection:
+                return mapped_intersection, remainders
+        return None, []
 
 
 def parse_seeds_part_1(f):
@@ -107,7 +111,7 @@ with open('input/day_05.txt') as file:
     mapping_stages = parse_mapping_stages(file)
 
 for stage in mapping_stages:
-    seeds = stage.translate(seeds)
+    seeds = stage.apply(seeds)
 
 print(min(map(lambda r: r.start, seeds)))
 
@@ -122,7 +126,7 @@ with open('input/day_05.txt') as file:
     mapping_stages = parse_mapping_stages(file)
 
 for stage in mapping_stages:
-    seeds = stage.translate(seeds)
+    seeds = stage.apply(seeds)
 
 print(min(map(lambda r: r.start, seeds)))
 
